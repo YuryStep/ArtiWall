cc63d7f7c0dc008df5d460a115467df8654c6e31
Yury Stepanchuk <stich-style@mail.ru>
2024-02-20 16:17:59 +0300

    Add a gradient background to the root view

--- ArtiWall/ViewController.swift
+++ ArtiWall/ViewController.swift
@@ -6,13 +6,26 @@
 //
 
 import UIKit
-import Lottie
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        setGradientBackground()
     }
 
+    private func setGradientBackground() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+
+        let topColor = UIColor.appPink.cgColor
+        let bottomColor = UIColor.black.cgColor
+        gradientLayer.colors = [topColor, bottomColor]
+
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+        gradientLayer.locations = [0, 0.2]
+
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
 }
