bb0e7cf7bb61f7817904937abddb84808308db17
Yury Stepanchuk <stich-style@mail.ru>
2024-02-20 23:56:54 +0300

    Add layout to the HomeView

--- ArtiWall/ViewController.swift
+++ ArtiWall/ViewController.swift
@@ -6,12 +6,48 @@
 //
 
 import UIKit
+import SnapKit
 
-class HomeViewController: UIViewController {
+final class HomeViewController: UIViewController {
+    private enum Constants {
+        static let offset = 10
+        static let inset = 10
+        static let searchBarHeight = 44
+        static let searchButtonImage = "arrow.up.circle"
+    }
+
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.layer.borderWidth = 3
+        searchBar.layer.borderColor = UIColor.white.cgColor
+        searchBar.keyboardAppearance = .dark
+        searchBar.tintColor = .white
+        searchBar.barTintColor = .clear
+        searchBar.searchTextField.defaultTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        searchBar.layer.cornerRadius = 15
+        searchBar.searchTextField.leftView = nil
+        searchBar.delegate = self
+        return searchBar
+    }()
+
+    private lazy var searchButton: UIButton = {
+        let button = UIButton(systemImage: Constants.searchButtonImage, imageFontSize: 30)
+        button.backgroundColor = .appPink
+        button.layer.cornerRadius = 15
+        button.layer.borderColor = UIColor.white.cgColor
+        button.addTarget(self, action: #selector(searchButtonTapped), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+    }
+
+    private func setupView() {
         setGradientBackground()
+        setupSubviews()
+        hideKeyboardOnTap(isOn: true)
     }
 
     private func setGradientBackground() {
@@ -27,5 +63,55 @@ class HomeViewController: UIViewController {
         gradientLayer.locations = [0, 0.2]
 
         view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
+    private func setupSubviews() {
+        view.addSubview(searchBar)
+        searchBar.addSubview(searchButton)
+
+        searchBar.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(Constants.offset)
+            $0.trailing.equalToSuperview().inset(Constants.inset)
+            $0.bottom.equalTo(view.keyboardLayoutGuide.snp.top).offset(-Constants.offset)
+            $0.height.equalTo(Constants.searchBarHeight)
+        }
+
+        searchButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(Constants.inset)
+        }
+    }
+
+    private func hideKeyboardOnTap(isOn: Bool) {
+        guard isOn else { return }
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(tapRecognizer)
+    }
+
+    @objc private func hideKeyboard() {
+        view.endEditing(true)
+    }
+
+    @objc private func searchButtonTapped() {
+        guard let text = searchBar.text, !text.isEmpty else { return }
+        print(text)
+        hideKeyboard()
+    }
+
+}
+
+extension HomeViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+         searchButtonTapped()
+     }
+}
+
+extension UIButton {
+    convenience init(systemImage: String, imageFontSize: CGFloat) {
+        self.init()
+        translatesAutoresizingMaskIntoConstraints = false
+        let systemImageConfiguration = UIImage.SymbolConfiguration(pointSize: imageFontSize, weight: .regular)
+        let image = UIImage(systemName: systemImage)?.withConfiguration(systemImageConfiguration)
+        setImage(image, for: .normal)
     }
 }
